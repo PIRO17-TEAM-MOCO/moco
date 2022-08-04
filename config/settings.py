d983210c02b1b2295c5c19528bcd7f523e5d41cb
chaenLIM <limce21@naver.com>
2022-08-04 16:17:27 +0900

    [feat] user 모델 구현

--- config/settings.py
+++ config/settings.py
@@ -82,6 +82,10 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'users.User'
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = ('email')
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -105,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 

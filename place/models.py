303999bb58494af58bab2fe1d6f9dfa8b2878b05
Younghoon Hwang <younghun.ai@gmail.com>
2022-08-05 17:43:06 +0900

    [feat] Place 모델 수정
    wifi 선택, 게시일자 추가

--- place/models.py
+++ place/models.py
@@ -13,15 +13,22 @@ class Place(models.Model):
         ('Less', '적음'),
         ('No', '없음'),
     ]
+    WIFI_CHOICE = [
+        ('Fast', '빠름'),
+        ('Slow', '느림'),
+        ('No', '없음'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=25)
     location = models.CharField(max_length=25)
     category = models.CharField(max_length=5, choices=CATEGORY_CHOICE)
     opening_time = models.TimeField()
     closing_time = models.TimeField()
-    wifi = models.BooleanField()
+    wifi = models.CharField(max_length=5, choices=WIFI_CHOICE)
     power_socket = models.CharField(max_length=5, choices=SOCKET_CHOICE)
     rating = models.FloatField()
     content = models.TextField()
+    published_at = models.DateTimeField(auto_now_add=True)
 
 class PlaceImage(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE)

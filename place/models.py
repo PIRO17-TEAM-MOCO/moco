29cd553f6fa5df1e85b77304e869299d278e7209
Younghoon Hwang <younghun.ai@gmail.com>
2022-08-05 16:42:59 +0900

    [feat] Place 모델 구현

--- place/models.py
+++ place/models.py
@@ -2,4 +2,27 @@ from django.db import models
 from users.models import User
 
 class Place(models.Model):
-    pass
+    CATEGORY_CHOICE = [
+        ('Cafe', '카페'),
+        ('StudyRoom', '스터디룸'),
+        ('Etc', '기타'),
+    ]
+    SOCKET_CHOICE = [
+        ('Many', '많음'),
+        ('Average', '보통'),
+        ('Less', '적음'),
+        ('No', '없음'),
+    ]
+    name = models.CharField(max_length=25)
+    location = models.CharField(max_length=25)
+    category = models.CharField(max_length=5, choices=CATEGORY_CHOICE)
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+    wifi = models.BooleanField()
+    power_socket = models.CharField(max_length=5, choices=SOCKET_CHOICE)
+    rating = models.FloatField()
+    content = models.TextField()
+
+class PlaceImage(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    image = models.ImageField()

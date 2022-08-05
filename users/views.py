00906fb848f830ae1bda7442e83dc505ff0b851d
Younghoon Hwang <younghun.ai@gmail.com>
2022-08-05 13:42:13 +0900

    [feat] 회원가입/로그인/로그아웃 구현

--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from .models import User
+from .forms import SignupForm
 
-# Create your views here.
+# url은 임시입니다
+def signup(request):
+  if request.method == 'POST':
+    form = SignupForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      auth.login(request, user)
+      return redirect('users:main')
+    return redirect('users:signup')
+  else:
+    form = SignupForm()
+    return render(request, 'signup.html', {'form' : form})
+
+def login(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    user = auth.authenticate(request, username=username, password=password)
+    if user is not None:
+      auth.login(request, user)
+      return redirect('users:main')
+    else:
+      return render(request, 'login.html')
+  else:
+    return render(request, 'login.html')
+
+def logout(request):
+  auth.logout(request)
+  return redirect("users:main")

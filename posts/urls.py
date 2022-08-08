aba893c1bc9fea7071da72ea6d1b9e3eedce0d02
nicholas1226 <dvc981226@gmail.com>
2022-08-08 14:00:58 +0900

    [feat]post 템플릿 임시 저장

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "posts"
 
@@ -11,4 +13,4 @@ urlpatterns = [
     path('update/<int:id>', views.update, name="update"),
     path("delete/<int:id>", views.delete, name="delete"),
     path("close/<int:id>", views.close, name="close"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

3d72ad1612f74055d3ffdaa50c72dd632afdc853
Younghoon Hwang <younghun.ai@gmail.com>
2022-08-09 15:19:14 +0900

    [feat] user 프로필 보기 및 수정 구현

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.forms import ModelForm
 from .models import User
 
 class SignupForm(UserCreationForm):
-  class Meta:
-    model = User
-    fields = ['username', 'email', 'password1', 'password2', 'name', 'nickname',
-            'profile_img', 'gender', 'job', 'birth', 'desc']
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2', 'name', 'nickname',
+                'profile_img', 'gender', 'job', 'birth', 'desc']
+
+class FindidForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['name', 'birth']
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['profile_img', 'nickname', 'gender', 'birth', 'job', 'desc']
--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('find_id/', views.find_id, name='find_id'),
+    path('profile/<int:id>', views.profile_view, name='profile_view'),
+    path('profile/edit/<int:id>', views.profile_edit, name='profile_edit'),
 ]

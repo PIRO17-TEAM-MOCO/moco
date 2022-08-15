41a3b46e5a1544717bd8fcd16a588d47fa25d7a1
Younghoon Hwang <younghun.ai@gmail.com>
2022-08-15 12:12:48 +0900

    [fix] user path 제거

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,6 @@ from django.conf.urls.static import static
 app_name = 'users'
 
 urlpatterns = [
-    path('', views.main, name='main'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),

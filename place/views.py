9dce0c5253735481cdffcfb94674e240d9664f5e
Younghoon Hwang <younghun.ai@gmail.com>
2022-08-21 15:24:42 +0900

    [style] 주석제거

--- place/views.py
+++ place/views.py
@@ -10,12 +10,6 @@ from users.views import profile_valid
 
 @profile_valid
 def home(request, category='None'):
-    # 프로필 유효성 검사
-    # if request.user.is_authenticated:
-    #     print('로그인됨')
-    #     if request.user.birth is None:
-    #         print('정보없음')
-    #         return redirect(f'/account/profile/add/{request.user.id}')
     # url에서 매개변수로 카테고리 받아옴
     # url에서 매개변수를 안 주면 'None'처리
     if category == 'all':

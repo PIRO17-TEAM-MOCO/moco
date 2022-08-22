01dd1dbe5d3b68597318d4d5f400c5dfe9f454c9
Younghoon Hwang <younghun.ai@gmail.com>
2022-08-22 19:24:26 +0900

    [feat] 닉네임 중복검사 오류

--- users/views.py
+++ users/views.py
@@ -51,6 +51,10 @@ def signup(request):
                 print('이미 존재하는 이메일입니다.')
                 messages.error(request, '이미 존재하는 이메일입니다.')
                 return redirect('users:signup_error')
+            if User.objects.filter(nickname=form.cleaned_data['nickname']).exists():
+                print('이미 존재하는 닉네임입니다.')
+                messages.error(request, '이미 존재하는 닉네임입니다.')
+                return redirect('users:signup_error')
             user = form.save()
             auth.login(request, user,
                        backend='django.contrib.auth.backends.ModelBackend')      
@@ -261,6 +265,10 @@ def profile_edit(request, id):
                 print('이미 존재하는 이메일입니다.')
                 messages.error(request, '이미 존재하는 이메일입니다.')
                 return redirect(f'/account/profile/edit/{id}')
+            if User.objects.filter(nickname=form.cleaned_data['nickname']).exclude(username=user.username).exists():
+                print('이미 존재하는 닉네임입니다.')
+                messages.error(request, '이미 존재하는 닉네임입니다.')
+                return redirect(f'/account/profile/edit/{id}')
             user.name = form.cleaned_data['name']
             user.nickname = form.cleaned_data['nickname']
             user.profile_img = form.cleaned_data['profile_img']
@@ -297,6 +305,10 @@ def profile_add(request, id):
             if User.objects.filter(email=form.cleaned_data['email']).exclude(username=user.username).exists():
                 print('이미 존재하는 이메일입니다.')
                 messages.error(request, '이미 존재하는 이메일입니다.')
+                return redirect(f'/account/profile/add/{id}')
+            if User.objects.filter(nickname=form.cleaned_data['nickname']).exclude(username=user.username).exists():
+                print('이미 존재하는 닉네임입니다.')
+                messages.error(request, '이미 존재하는 닉네임입니다.')
                 return redirect(f'/account/profile/add/{id}')
             user.name = form.cleaned_data['name']
             user.nickname = form.cleaned_data['nickname']

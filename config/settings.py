b82473fd2ce0646862ad390babec9ba4a536ae08
Younghoon Hwang <younghun.ai@gmail.com>
2022-08-22 23:35:50 +0900

    [fix] 오류 수정

--- config/settings.py
+++ config/settings.py
@@ -152,9 +152,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
 
 
 

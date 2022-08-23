54d8beaac8906e39f4d731bbfac35106adb54534
Younghoon Hwang <younghun.ai@gmail.com>
2022-08-23 14:14:08 +0900

    [docs] settings.py 배포용 수정

--- config/settings.py
+++ config/settings.py
@@ -29,12 +29,13 @@ if env_path.exists():
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6#&qcjh05*)4(lx9=63v@zueeq%q5!$92+2i4yid(p8429o91('
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    'http://ec2-3-34-125-211.ap-northeast-2.compute.amazonaws.com/',
 ]
 
 

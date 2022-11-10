bf46b73e0680437c8b1ad9e4d361415391cda10b
chaenLIM <limce21@naver.com>
2022-11-10 23:25:40 +0900

    [refactor] 모각코글 리팩토링

--- posts/models.py
+++ posts/models.py
@@ -2,9 +2,6 @@ from email.mime import image
 from django.db import models
 from users.models import User
 
-# Create your models here.
-
-
 class Post(models.Model):
     CONTACT_CHOICE = [
         ('On', '온라인'),
@@ -15,27 +12,24 @@ class Post(models.Model):
         ('Regular', '정기'),
         ('OneTime', '번개')
     ]
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE)  # 유저가 삭제되면 작성한 글도 삭제됨
-    title = models.CharField(max_length=50)  # 제목
-    published_at = models.DateTimeField(auto_now_add=True)  # 게시일자
-    location = models.CharField(max_length=20)  # 지역 -> XX시 XX구
-    contact = models.CharField(max_length=10, choices=CONTACT_CHOICE)  # 온, 오프
-    number = models.IntegerField()  # 인원 -> 최소2명
-    tag = models.CharField(max_length=250, blank=True, null=True)  # 파이썬, 알고리즘
-    content = models.TextField()  # 내용
-    apply_link = models.CharField(max_length=100)  # 구글폼 링크가 더 길면 수정
-    views = models.IntegerField(default=0)  # 조회수
-    duration = models.CharField(
-        max_length=10, choices=DURATION_CHOICE)  # 정기, 번개
-    activation = models.BooleanField(default=True)  # True : 모집중, False : 모집완료
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  
+    title = models.CharField(max_length=50) 
+    published_at = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=20)
+    contact = models.CharField(max_length=10, choices=CONTACT_CHOICE)
+    number = models.IntegerField()
+    tag = models.CharField(max_length=250, blank=True, null=True) 
+    content = models.TextField()
+    apply_link = models.CharField(max_length=100) 
+    views = models.IntegerField(default=0)
+    duration = models.CharField(max_length=10, choices=DURATION_CHOICE) 
+    activation = models.BooleanField(default=True)
     likes = models.IntegerField(default=0)
 
 
 class Review(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)    # 후기 작성 유저
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)    # 후기를 다는 Post
-    image = models.ImageField(blank=True, null=True,
-                              upload_to='post/reviews/%Y%m%d')    # 모임 인증 이미지
-    content = models.TextField()    # 내용
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    post = models.ForeignKey(Post, on_delete=models.CASCADE) 
+    image = models.ImageField(blank=True, null=True, upload_to='post/reviews/%Y%m%d')  
+    content = models.TextField() 
     write_at = models.DateTimeField(auto_now_add=True)
